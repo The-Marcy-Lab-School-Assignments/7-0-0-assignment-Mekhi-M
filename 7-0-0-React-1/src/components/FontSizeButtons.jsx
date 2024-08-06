4d1980e4957a43be23724f7b91c72cf75836cfc1
Mekhi-M <mekhim0731@gmail.com>
2024-08-05 22:29:14 -0400

    well now i know its due wednesday

---
+++ 7-0-0-React-1/src/components/FontSizeButtons.jsx
@@ -0,0 +1,8 @@
+export const FontSizerButtons = ({increment, decrement}) => {
+  return (
+    <section style={{display: 'flex', justifyContent: 'center', gap: '5rem'}}>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </section>
+  )
+}
